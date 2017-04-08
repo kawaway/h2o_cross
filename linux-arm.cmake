9c42daf03e8bc6de1fa1bf9a1ed1f7f2162a4f40
kawaway <kawawa.y@gmail.com>
2017-04-08 10:32:54 +0000

    Delete environmet dependency code

--- linux-arm.cmake
+++ linux-arm.cmake
@@ -2,7 +2,7 @@ set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR arm)
 
 set(CMAKE_SYSROOT /opt/rpi/rootfs)
-set(CMAKE_STAGING_PREFIX /home/data/target)
+set(CMAKE_STAGING_PREFIX ${CMAKE_INSTALL_PREFIX})
 
 set(tools /opt/rpi/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64)
 set(CMAKE_C_COMPILER ${tools}/bin/arm-linux-gnueabihf-gcc)
